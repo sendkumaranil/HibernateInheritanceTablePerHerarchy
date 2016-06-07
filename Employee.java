771a4ff809e36cdb20739b7f6ae933d9b52e46fe
Anil Kumar <sendkumaranil@gmail.com>
2016-06-07 16:17:56 +0530

    POJO class of Employee Entity where relationship defined with annotataion

---
+++ Employee.java
@@ -0,0 +1,43 @@
+package com.hibernate.inheritance.tabperhirarchy.model;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee_details")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="disc_type",discriminatorType=DiscriminatorType.STRING)
+@DiscriminatorValue(value="Employee")
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
